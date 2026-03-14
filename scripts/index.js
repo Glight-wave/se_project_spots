ccc83020b3405b2428fc2d2105ea097e8b55cf9b
Glight <lightingthewave@outlook.com>
2026-03-14 14:32:35 -0500

    fix edit profiles description prefill

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,28 @@
 const editProfileOpenBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input",
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input",
+);
 
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-
 const newPostBtn = document.querySelector(".profile__add-btn");
 
+const profileNameEL = document.querySelector(".profile__name");
+const profileDescriptionEL = document.querySelector(".profile__description");
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const nameInput = addCardFormElement.querySelector("#card-caption-input");
+const linkInput = addCardFormElement.querySelector("#card-image-input");
+
 editProfileOpenBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEL.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEL.textContent;
   editProfileModal.classList.add("modal_opened");
 });
 
@@ -16,9 +31,29 @@ editProfileCloseBtn.addEventListener("click", function () {
 });
 
 newPostBtn.addEventListener("click", function () {
+  nameInput.value = "";
+  linkInput.value = "";
   newPostModal.classList.add("modal_opened");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_opened");
 });
+
+function handleEditProfileSubmit(event) {
+  event.preventDefault();
+  profileNameEL.textContent = editProfileNameInput.value;
+  profileDescriptionEL.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_opened");
+}
+
+function handleAddCardSubmit(event) {
+  event.preventDefault();
+  // Logic to add a new card goes here
+  newPostModal.classList.remove("modal_opened");
+}
+
+EventTarget.preventDefault();
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
